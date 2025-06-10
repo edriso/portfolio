241170558824a052cd1279859fdebc7e9f2807df
Mohamed Idris <edriso.dev@gmail.com>
2025-06-10 23:26:13 +0300

    Add kudos from Small Improvements and Crowdbonus

--- src/views/Kudos.vue
+++ src/views/Kudos.vue
@@ -1,11 +1,23 @@
 <script>
+import axios from "axios";
+import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+
 export default {
   name: "Kudos",
-  data() {
+  setup() {
+    // Data
+    const kudos = ref([]);
+
+    // Lifecycle Hooks
+    onMounted(() => {
+      axios
+        .get("../db/kudos.json")
+        .then((res) => (kudos.value = res.data.slice().reverse()));
+    });
+
     return {
-      kudos: [
-        "hello there",
-      ],
+      kudos,
     };
   },
 };
@@ -13,11 +25,21 @@ export default {
 
 <template>
   <main class="kudos">
-    <p>During my time at <a href="https://scandiweb.com" target="_blank" rel="noopener noreferrer">Scandiweb</a>, I had the chance to use two great internal apps. One is <a href="https://scandiweb.small-improvements.com" target="_blank" rel="noopener noreferrer">Small Improvements</a>, where team leads and colleagues provide feedback, and the other is <a href="https://crowdbonus.com" target="_blank" rel="noopener noreferrer">Crowdbonus</a>, which allows employees to share 50 euros as a token of appreciation. Below are some of the feedback and recognition I received through these platforms.</p>
+    <p class="kudos__intro">During my time at <a href="https://scandiweb.com" target="_blank" rel="noopener noreferrer">Scandiweb</a>, I had the chance to use two great internal apps. One is <a href="https://scandiweb.small-improvements.com" target="_blank" rel="noopener noreferrer">Small Improvements</a>, where team leads and colleagues give feedback, and the other is <a href="https://crowdbonus.com" target="_blank" rel="noopener noreferrer">Crowdbonus</a>, which allows employees to share 50 euros as a way of saying thanks. Below are some of the feedback and recognition I received through these platforms.</p>
 
     <ul class="kudos__content">
-      <li v-for="(kudo, i) in kudos" :key="i">
-        <span>{{ kudo }}</span>
+      <li
+        v-for="kudo in kudos"
+        :key="kudo.id"
+        class="kudos__item flex flex-col flex-ai-c flex-jc-c flex-gap-sm text-center"
+      >
+        <blockquote class="kudos__quote">
+          <span class="kudos__quote-mark">&#8220;</span>
+          <span class="kudos__quote-text">{{ kudo.content }}</span>
+          <span class="kudos__quote-mark">&#8221;</span>
+        </blockquote>
+
+        <p class="kudos__date" v-if="kudo.date">{{ kudo.date }}</p>
       </li>
     </ul>
   </main>
